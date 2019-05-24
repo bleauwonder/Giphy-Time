9b27353658661272068dc5cd125ec8b97d8eaaa1
bleauwonder <elyzableau.om@gmail.com>
2019-05-24 18:47:30 -0400

    mixed last part of object for still so now it runs

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,12 +1,11 @@
 // initial Sitcom array
-var topics = ["The Good Place", "The Goldbergs", "Letterkenny", "Taxi", "I Love Lucy"];
+var topics = ["The Good Place", "The Goldbergs", "Letterkenny", "How I Met Your Mother", "I Love Lucy"];
 
 function displaySitcom() {
     var sitcom = $(this).attr("data-name");
 	console.log("TCL: displaySitcom -> sitcom", sitcom)
     
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sitcom + "&api_key=xQ1cEV48oeuAx0ibvev8eMOowJYJ5xPL&limit=10";
-// how to really get gif from URL in the API without embedding
 
     $.ajax({
         url: queryURL,
@@ -14,24 +13,18 @@ function displaySitcom() {
     }).then(function(response) {
         console.log("TCL: displaySitcom -> response", response)
         
-        var sitcomDiv = $("<div class='sitcom'>");
-        
         for (var i = 0; i < response.data.length; i++) {
-        var rating = response.data[i].rating;
+        var rating = "Rating: " + (response.data[i].rating).toUpperCase();
         var active = response.data[i].images.fixed_width.url;
-        var still = response.data[i].images.fixed_width_still;
-        
-        // var p = $("<p>").text("Rating: " + rating);
-        // sitcomDiv.append(p);
-        // $("#sitcom-view").prepend(sitcomDiv);
+        var still = response.data[i].images.fixed_width_still.url;
         
         var gifImage = $("<img>");
         var ratingDiv = $("<div id='ratingDiv'>" + rating + "</div>");
 
         gifImage.attr({"active": active, "still": still, "src": still, "state": "still"});
 
-        var ratingAndImage = $("<div>");
-        $(ratingAndImage).css({"float":"left"});
+        var ratingAndImage = $("<div id='ratingAndImage'>");
+        
         $(ratingAndImage).prepend(ratingDiv, gifImage);
         $("#sitcom-view").prepend(ratingAndImage);
 
@@ -47,7 +40,6 @@ function displaySitcom() {
                 $(this).attr("state", "still");
             }
         });
-        // sitcomDiv.append(animated);
         }
     });
 }
